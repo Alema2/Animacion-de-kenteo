df7da0406086799cb7b3905dd75347d5a329fe94
salinas <salinas@sali.sali>
2025-02-27 21:04:01 -0500

    salinas: fondo-kenteo

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,42 +1,26 @@
-import React, { useRef } from "react";
-import * as THREE from "three"; // Importamos Three.js para materiales
-import { useFrame, useLoader } from "@react-three/fiber";
-import { TextureLoader } from "three";
-
+import React, { useRef, useEffect } from "react";
+import { useLoader, useThree } from "@react-three/fiber";
+import * as THREE from "three";
 
 const Background = () => {
-  const floorRef = useRef();
+  const texture = useLoader(THREE.TextureLoader, "/assets/water.png");
+  const meshRef = useRef();
+  const { viewport, size } = useThree(); // Obtener el tamaño de la ventana
 
-  const displacementMap = useLoader(TextureLoader, "/assets/water.jpg");
-
-
-  useFrame(({ clock }) => {
-    if (floorRef.current) {
-      const material = floorRef.current.material;
-      material.displacementScale = Math.sin(clock.elapsedTime) *0.5
-      material.color.setHSL(Math.sin(clock.elapsedTime) * 0.1 + 0.5, 0.5, 0.4);
+  useEffect(() => {
+    if (texture) {
+      texture.minFilter = THREE.LinearFilter;
+      texture.magFilter = THREE.LinearFilter;
+      texture.wrapS = THREE.RepeatWrapping;
+      texture.wrapT = THREE.RepeatWrapping;
     }
-  });
+  }, [texture]);
 
   return (
-    <>
-      {/* Pared (fondo estático) */}
-      <mesh position={[0, 0, -5]} receiveShadow>
-        <planeGeometry args={[10, 10]} />
-        <meshStandardMaterial color="#8a8a8a" />
-      </mesh>
-
-      {/* Piso con efecto de ondas */}
-      
-      <mesh ref={floorRef} rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]} receiveShadow>
-        <planeGeometry args={[10, 10, 50, 50]} />
-        <meshStandardMaterial
-          color={"#4a4a4a"}
-          displacementMap={displacementMap} // Agregar mapa de desplazamiento
-          displacementScale={0.3} // Controlar la intensidad del relieve
-        />
-      </mesh>
-    </>
+    <mesh ref={meshRef} scale={[viewport.width, viewport.height, 1]}>
+      <planeGeometry args={[1, 1]} />
+      <meshBasicMaterial map={texture} side={THREE.DoubleSide} />
+    </mesh>
   );
 };
 
